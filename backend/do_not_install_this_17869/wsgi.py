55b0e398f92d2315807cbd3a1789d9aea05e6c79
Crowdbotics <team@crowdbotics.com>
2020-06-05 23:46:09 +0000

    Update models

--- backend/do_not_install_this_17869/wsgi.py
+++ backend/do_not_install_this_17869/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'do_not_install_this_17869.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "do_not_install_this_17869.settings")
 
 application = get_wsgi_application()
